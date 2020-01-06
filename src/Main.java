7a805aa3ea249da1ee491cc37ace56060ad5a4d5
unknown <zhaokw@outlook.com>
2020-01-05 20:51:05 -0500

    Create Project

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+	public static String extractNumberFormat(String input) {
+		String USPhonePat = "\\s*(?:\\(\\s*(\\d{3})\\s*\\)|(\\d{3}))\\s*\\-?\\s*(\\d{3})\\s*\\-?\\s*(\\d{4})\\s*"; 
+		
+		if (!input.matches(USPhonePat))
+			return "Not Valid US Phone";
+		
+		Matcher m = Pattern.compile(USPhonePat).matcher(input); m.find();
+		return "(" + (input.indexOf('(') >=0 ? m.group(1) : m.group(2)) + ")-" + m.group(3) + "-" + m.group(4);
+	}
+	
+	public static void main(String[] args) {
+		// Case 1: Match, with area code in parenthesis
+		String NYU = "(212)  -998  1212";
+		System.out.println(extractNumberFormat(NYU));
+		
+		// Case 2: Match		
+		NYU = "212 9981212";
+		System.out.println(extractNumberFormat(NYU));
+		
+		// Case 3: Match		
+		NYU = "212  998 - 1212";
+		System.out.println(extractNumberFormat(NYU));
+		
+		// Case 4: Not match: area code is not enclosed in parenthesis		
+		NYU = "(212  -998 1212";
+		System.out.println(extractNumberFormat(NYU));
+		
+		// Case 5: Not match: hyphen is not positioned correctly		
+		NYU = "(212)-9981-212";
+		System.out.println(extractNumberFormat(NYU));
+		
+		// Case 5: Not match: length is too short for US number		
+		NYU = "(212)-99-212";
+		System.out.println(extractNumberFormat(NYU));		
+		
+	}
+}
